e7e5b2c2a32bfaf73efef92d3cd6fbf579bb33f1
Cain-Sam <108156831+Cain-Sam@users.noreply.github.com>
2026-09-12 11:29:07 -0700

    Pieces break doors!
    New collider group

--- Godot Test Projects/new-test/Scenes/kickedObject.gd
+++ Godot Test Projects/new-test/Scenes/kickedObject.gd
@@ -1,6 +1,25 @@
 extends RigidBody3D
+var velocity_last_frame: Vector3 = Vector3.ZERO
 
 func kicked(direction, intensity):
 	direction.y += 1
 	intensity += 10
 	self.apply_impulse( direction * intensity, self.global_position )
+	
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var contact_count = state.get_contact_count()
+	
+	for i in range(contact_count):
+		var total_impulse := Vector3.ZERO
+		var impulse := state.get_contact_impulse(i)
+		total_impulse += impulse
+		
+		var impact_strength := total_impulse.length()
+		
+		var hit_object = state.get_contact_collider_object(i)
+		var hit_from_direction = state.get_contact_local_normal(i)
+		
+		if hit_object && state.get_contact_collider_object(i).has_method("collided") && total_impulse.length() > 0.2:		
+			state.get_contact_collider_object(i).collided(hit_from_direction, total_impulse.length() + 2)
+			print (str(hit_from_direction))
+		
--- Godot Test Projects/new-test/Scripts/door_rigid.gd
+++ Godot Test Projects/new-test/Scripts/door_rigid.gd
@@ -1,7 +1,15 @@
-extends StaticBody3D
+extends RigidBody3D
 const DOOR_PIECES_MODIFIER = preload("uid://cdeodg3s1run2")
 
 func kicked(direction, intensity):
+	var broken_model_inst = DOOR_PIECES_MODIFIER.instantiate();
+	broken_model_inst.direction = direction
+	broken_model_inst.intensity = intensity	
+	get_parent().add_child(broken_model_inst)
+	broken_model_inst.global_transform = self.global_transform;
+	self.queue_free();
+
+func collided(direction, intensity):
 	var broken_model_inst = DOOR_PIECES_MODIFIER.instantiate();
 	broken_model_inst.direction = direction
 	broken_model_inst.intensity = intensity	
