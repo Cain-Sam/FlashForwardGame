c6fcc5c466deb83217f8327717b2a02328dddcde
Cain-Sam <108156831+Cain-Sam@users.noreply.github.com>
2026-09-14 08:22:11 -0700

    Moved gun raycast back
    It was shooting through objects this should avoid that

--- Godot Test Projects/new-test/Scripts/bullet.gd
+++ Godot Test Projects/new-test/Scripts/bullet.gd
@@ -12,7 +12,6 @@ var stuck := false
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if stuck:
