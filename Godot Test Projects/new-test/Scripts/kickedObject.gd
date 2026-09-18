ea95320b0ec9a4a23bfe0162d316963b7034091d
Cain-Sam <108156831+Cain-Sam@users.noreply.github.com>
2026-09-18 10:10:28 -0700

    Physics fixes for curtain

--- Godot Test Projects/new-test/Scripts/kickedObject.gd
+++ Godot Test Projects/new-test/Scripts/kickedObject.gd
@@ -29,3 +29,15 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		if hit_object && state.get_contact_collider_object(i).has_method("collided"):		
 			state.get_contact_collider_object(i).collided(hit_from_direction, total_impulse.length() + 2)
 		
+func _physics_process(delta: float) -> void:
+	if linear_velocity.length_squared() < 0.5:
+		return
+	var next_position = global_position + (linear_velocity * delta)
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, next_position)
+	query.collision_mask = collision_mask 
+	var result = space_state.intersect_ray(query)
+	if result and result.collider is SoftBody3D:
+		global_position = result.position - (linear_velocity.normalized() * 0.05)
+		linear_velocity = linear_velocity * 0.15
+		linear_velocity.y -= 2.0 
